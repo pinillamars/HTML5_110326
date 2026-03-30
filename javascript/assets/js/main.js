1313c1ddfceb70228859215da15a250450300d9c
Mars <pinilla.mars@gmail.com>
2026-03-30 10:22:05 +0200

    js test

---
+++ javascript/assets/js/main.js
@@ -0,0 +1,8 @@
+// alert("Hello world!");
+const NOMBREUSER = "Mars";
+const EDAD = "20";
+const TELEFONO = "+34 576 887 746"
+const CUENTAACTIVA= true;
+let hobbies = ["programar", "jugar a videojuegos", "natación"];
+let poblacion = null;
+const DNI_NIF = "57486900F";
