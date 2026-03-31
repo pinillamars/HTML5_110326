d77c80b02289e747e8e228d6aac9cb3f4406015a
Mars <pinilla.mars@gmail.com>
2026-03-31 10:43:42 +0200

    reto 1 js clase

--- 310326_javascript/assets/js/mainReto.js
+++ 310326_javascript/assets/js/mainReto.js
@@ -1,19 +1,15 @@
 for (let i = 1; i<= 10; i++){
     if (i%4 ===0 && i%6===0){
         document.writeln("ALERTA CRÍTICA");
-        document.writeln("<br>");
     }else if (i%4 ===0){
         document.writeln("Chequeo de seguridad");
-        document.writeln("<br>");
     }else if (i%6===0){
         document.writeln("Alerta del sistema");
-        document.writeln("<br>");
     }else if (i%9===0){
         document.writeln("Sistema bloqueado");
-        document.writeln("<br>");
         break;
     }else {
         document.writeln("Ciclo "+i," operativo")
-        document.writeln("<br>");
     }
+    document.writeln("<br>");
 }
